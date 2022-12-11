02f5f46aa39a4354c81de4716af8db9b2053ec0e
can balcı <canbalci94@hotmail.com>
2022-12-12 00:16:43 +0300

    12.12.2022

---
+++ MvcCv/Controllers/YetenekController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCv.Models.Entitiy;
+using MvcCv.Repository;
+
+namespace MvcCv.Controllers
+{
+    public class YetenekController : Controller
+    {
+        GenericRepository<TblYeteneklerim> repo = new GenericRepository<TblYeteneklerim>();
+        public ActionResult Index()
+        {
+            var yetenekeler = repo.List();  
+            return View(yetenekeler);
+        }
+        [HttpGet]
+        public ActionResult YeniYetenek()
+        {
+            return View();  
+        }
+        [HttpPost]
+        public ActionResult YeniYetenek(TblYeteneklerim p)
+        {
+            repo.TAdd(p);
+            return RedirectToAction("Index");
+        }
+        public ActionResult YetenekSil(int id)
+        {
+            var yetenek = repo.Find(x=> x.ID == id);
+            repo.TDelete(yetenek);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult YetenekDuzenle(int id )
+        {
+            var yetenek = repo.Find(x=>x.ID == id);           
+            return View(yetenek);
+        }
+        [HttpPost]
+        public ActionResult YetenekDuzenle(TblYeteneklerim t)
+        {
+            var y = repo.Find(x => x.ID == t.ID);
+            y.Yetenek = t.Yetenek;
+            y.Oran = t.Oran;
+            repo.TUpdate(y);
+            return RedirectToAction("Index");
+        }
+    }
+}
